06485b00b4278dc86ddf404bc12ac980eb86b598
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-10-25 21:55:29 +0900

    project init

---
+++ pareum/src/main/java/twkim/pareum/PareumApplication.java
@@ -0,0 +1,13 @@
+package twkim.pareum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PareumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PareumApplication.class, args);
+	}
+
+}
---
+++ pareum/src/test/java/twkim/pareum/PareumApplicationTests.java
@@ -0,0 +1,13 @@
+package twkim.pareum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PareumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
